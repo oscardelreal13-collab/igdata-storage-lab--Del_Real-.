9a6d2d1e26072a79cb7b1e1b2d06b154c3de8536
oscardelreal13-collab <oscardelreal13@gmail.com>
2025-09-24 14:44:16 +0200

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title("Big Data Storage Lab - <apellido>")
-st.write("Bienvenido a la aplicación de visualización de datos Silver 🚀")
-
-# Aquí se podrán cargar y mostrar tablas + KPIs
